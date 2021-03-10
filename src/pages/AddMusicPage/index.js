731c66662c5363b0f65c8cfbe500ad1564e7bcd0
Noh Ah Jeong <noh_ah@hotmail.com>
2021-03-10 09:37:22 -0300

    add music page

--- src/pages/AddMusicPage/index.js
+++ src/pages/AddMusicPage/index.js
@@ -1,8 +1,121 @@
+import useForm from '../../hooks/useForm'
+import axios from 'axios'
+import { BASE_URL } from '../../constants/urls'
+import { useHistory } from 'react-router-dom'
+import { TextField } from '@material-ui/core'
+import { Button } from '@material-ui/core'
+import Header from '../../components/Header'
+import { AddMusicPageContainer, FormStyled, RowFlexDiv } from './styles'
+import useProtectedPage from '../../hooks/useProtectedPage'
+
 const AddMusicPage = () => {
-    return(
-        <div>
-            AddMusic
-        </div>
+    useProtectedPage()
+    const history = useHistory()
+
+    const { form, onChangeInput } = useForm({
+        title: '',
+        author: '',
+        date: '',
+        file: '',
+        album: ''
+    })
+
+    const onSubmit = (event) => {
+        event.preventDefault()
+
+        const body = {
+            title: form.title,
+            author: form.author,
+            date: form.date,
+            file: form.file,
+            album: form.album,
+            genre: []
+        }
+        axios
+            .post(`${BASE_URL}/add`, body, {
+                headers: {
+                    Authorization: window.localStorage.getItem('token')
+                }
+            })
+            .then((response) => {
+                alert('Music added')
+
+                history.push('/feed')
+            })
+            .catch((error) => {
+                alert(error.response.data.error)
+            })
+    }
+
+    return (
+        <AddMusicPageContainer>
+            <Header />
+
+            <FormStyled onSubmit={onSubmit}>
+                <TextField
+                    label='Title'
+                    type='text'
+                    onChange={onChangeInput}
+                    value={form.title}
+                    name={'title'}
+                />
+
+                <RowFlexDiv>
+                    <TextField
+                        label='Author'
+                        type='text'
+                        onChange={onChangeInput}
+                        value={form.author}
+                        name={'author'}
+                        fullWidth
+                    />
+
+                    <TextField
+                        label='Album'
+                        type='text'
+                        onChange={onChangeInput}
+                        value={form.album}
+                        name={'album'}
+                        fullWidth
+                    />
+                </RowFlexDiv>
+
+                <TextField
+                    label='Genre'
+                    type='text'
+                    onChange={onChangeInput}
+                    value={form.genre}
+                    name={'genre'}
+                    fullWidth
+                />
+
+                <RowFlexDiv>
+                    <TextField
+                        type='date'
+                        onChange={onChangeInput}
+                        value={form.date}
+                        name={'date'}
+                    />
+
+                    <TextField
+                        type='file'
+                        onChange={onChangeInput}
+                        value={form.file}
+                        name={'file'}
+                        accept='audio/*'
+                    />
+                </RowFlexDiv>
+
+                <Button
+                    variant='contained'
+                    color='primary'
+                    type='submit'
+                >
+                    add
+                </Button>
+            </FormStyled>
+
+        </AddMusicPageContainer>
     )
 }
 
---
+++ src/pages/AddMusicPage/styles.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components'
+
+export const AddMusicPageContainer = styled.div`
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+`
+
+export const FormStyled = styled.form`
+    margin: 2rem;
+    padding: .8rem;
+    border-radius: .6rem;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    background-color: whitesmoke;
+`
+
+export const RowFlexDiv = styled.div`
+    width: calc(100vw-5.2rem);
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+`
